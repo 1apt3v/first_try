298cc03b7b2715ced7727aa328ceba7856281d6a
Nikita Istomin <nikita@mmax.ru>
2021-05-14 21:19:26 +0500

    temp commit for fix problems with images

--- first_try/src/components/footer/Footer.jsx
+++ first_try/src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import style from "./Footer.module.css"
-import imageSrc from './../../img/myImages.png'
+// import imageSrc from './../../img/myImages.png'
+
+const image = require('./img/myImage.png')
 
 const Footer = () => {
     return (
         
         <div className={style.footer}>
-            
+            <img src={image} alt="" />
             <div className="list">
                 <div className="item">
                     <a href="mailto:nikita@mmax.ru"></a>
